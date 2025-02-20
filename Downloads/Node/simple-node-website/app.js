5c8166832409926c00ede5c6df8f569b012b0d1a
NirmalPadvi26 <nirmalp2632@gmail.com>
2025-02-20 10:28:05 +0530

    Initial commit with package.json and Procfile

--- Downloads/Node/simple-node-website/app.js
+++ Downloads/Node/simple-node-website/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Define a basic route
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is my simple Node.js website.');
+    res.send('Hello World!');
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
